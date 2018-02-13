14d8e0107a3e0236b2698f202bc3bde74f0650cc
Athelor <atlonpc@ukr.net>
2018-02-13 07:52:09 +0200

    Update select.sh

--- select.sh
+++ select.sh
@@ -3,6 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export WINEPREFIX="$DIR/prefix"
 export WINEARCH="win64"
 export WINE="/usr/bin/wine"
-wine "`zenity --file-selection --title="Select a File"`"
-
-
+export PATH="`zenity --file-selection --title="Select a File"`"
+wine ${PATH}
