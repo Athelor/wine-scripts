ab23fd1b771ad13d0ffe477cf7cf4ef34124d76e
Athelor <atlonpc@ukr.net>
2018-02-23 16:34:35 +0200

    Update winetricks.sh

--- winetricks.sh
+++ winetricks.sh
@@ -2,8 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export WINEPREFIX="$DIR/prefix"
 export WINEARCH="win64"
-export winetricks="./winerticks"
+export winetricks="./winetricks"
 export WINE="/usr/bin/wine"
-${winetricks}
+"${winetricks}"
 
 
